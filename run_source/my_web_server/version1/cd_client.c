2b3ff23dcf9e9ac7e6ef812d32ecac2463b942c4
caida <caida@mafengwo.com>
2018-03-21 14:46:48 +0800

    v1版本

--- run_source/my_web_server/version1/cd_client.c
+++ run_source/my_web_server/version1/cd_client.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
@@ -18,9 +17,9 @@
 int main(int argc, char *argv[]){
 
     /*定义使用中的变量*/
-    int sfd;                                    //服务器端和客户端的socket描述符
+    int sfd;                                    //客户端的socket描述符
     char buf[SIZEBUF];                          //读取写入buf的大小
-    int len;                                    //读取长度和循环因子
+    int len;                                    //读取长度
     struct sockaddr_in serv_addr, cli_addr;     //客户端和服务器端bind结构体
     char clie_ip[SIZEBUF], serv_ip[SIZEBUF];    //保存打印信息ip的字符数组
 
@@ -31,16 +30,15 @@ int main(int argc, char *argv[]){
         exit(1);
     }
 
-
     /*绑定服务器地址结构*/
-    socklen_t serv_len;                //结构体长度
+    socklen_t serv_len;                         //结构体长度
     serv_len = sizeof(serv_addr);               //获取结构体长度
     memset(&serv_addr, 0, serv_len);            //清空结构体
     serv_addr.sin_family = AF_INET;             //使用的协议族
-    inet_pton(AF_INET, SERV_IP, &serv_addr.sin_addr.s_addr);       //本机的任何网卡
-    serv_addr.sin_port = htons(SERV_PORT);      //程序端口号，本地转网络字节序【为0，则系统自动分配，使用getsockname函数配合】
+    inet_pton(AF_INET, SERV_IP, &serv_addr.sin_addr.s_addr);  //指定IP 字符串类型转换为网络字节序 参3:传出参数
+    serv_addr.sin_port = htons(SERV_PORT);      //指定端口 本地转网络字节序
 
-    //connect
+    /*根据地址结构链接指定服务器进程*/
     if(connect(sfd, (struct sockaddr *) &serv_addr, serv_len) == -1){
         perror("fail to create socket");
         exit(1);
@@ -60,13 +58,12 @@ int main(int argc, char *argv[]){
         /*从标准输入获取数据*/
         fgets(buf, sizeof(buf), stdin);
         /*将数据写给服务器*/
-        write(sfd, buf, strlen(buf));       //写个服务器
+        write(sfd, buf, strlen(buf));
         /*从服务器读回转换后数据*/
         len = read(sfd, buf, sizeof(buf));
         /*写至标准输出*/
         write(STDOUT_FILENO, buf, len);
     }
-
     /*关闭链接*/
     close(sfd);
     return 0;
